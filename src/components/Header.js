0c70af3c7d4e1dce571c8fe5d481767d697e2cd0
KushagraShukla30 <1ds23cd027@dsce.edu.in>
2024-11-17 00:54:30 +0530

    Top Ribbon Changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,54 @@
-import React from "react"
-import styled from "styled-components"
-import { Avatar } from "@material-ui/core"
+import React from "react";
+import styled from "styled-components";
+import { Avatar } from "@material-ui/core";
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import SearchIcon from '@mui/icons-material/Search';
 
 function Header() {
   return (
     <HeaderContainer>
-        <h1>Hello</h1>
-        <HeaderLeft>
-          <HeaderAvatar            
-          />
-          <AccessTimeIcon
-          />
-        </HeaderLeft>
-        
+      <HeaderLeft>
+        <HeaderAvatar />
+        <AccessTimeIcon />
+      </HeaderLeft>
+      <HeaderSearch>
+        <SearchIcon />
+      </HeaderSearch>
     </HeaderContainer>
   );
 }
 
 export default Header;
 
-const HeaderContainer = styled.div``;
+const HeaderSearch = styled.div``
 
-const HeaderLeft = styled.div``;
+const HeaderContainer = styled.div`
+display: flex;
+position: fixed;
+width: 100%;
+align-items: center;
+justify-content: space-between;
+padding: 10px 0;
+background-color: var(--peerloop-color);
+color: white;
 
-const HeaderAvatar = styled(Avatar)``
+`
 
+const HeaderLeft = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+
+  > .MuiSvgIcon-root {
+    margin-right: 10px;
+  }
+`;
+
+const HeaderAvatar = styled(Avatar)`
+  cursor: pointer;
+  :hover {
+    opacity: 0.8;
+  }
+`;
